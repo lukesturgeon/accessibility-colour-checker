d15c825e5e240ca2712bc859729da43b25b28931
Luke Sturgeon <939195+lukesturgeon@users.noreply.github.com>
2024-03-12 11:21:47 +0000

    load base from .env file to build locally for production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd())
+  return {
+    // vite config
+    base: env.VITE_BASE,
+    plugins: [vue()],
+  }
 })
